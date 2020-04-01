5f516ae091b04296f6a930d696b1d9f77fed14be
changpowei <ld5874123@gmail.com>
2020-04-01 16:33:28 +0800

    Kernel Inception.py的參數改為外部輸入

--- Kernel Inspection.py
+++ Kernel Inspection.py
@@ -11,13 +11,21 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2
+import argparse
 
-# Layer name to inspect
-layer_name = 'block5_conv1'
+parser = argparse.ArgumentParser()
 
-epochs = 150
-step_size = 1.
-filter_index = 0
+parser.add_argument("-l", "--layer_name", default = 'block5_conv1', type = str, help = "Kernel from which layer?")
+parser.add_argument("-e", "--epochs", default = 150, type = int, help = "How many epoches?")
+parser.add_argument("-s", "--step_size", default = 1., type = float, help = "Like learning rate!")
+parser.add_argument("-i", "--filter_index", default = 0, type = int, help = "which Kernel from the layer?")
+args = parser.parse_args()
+
+layer_name = args.layer_name
+epochs = args.epochs
+step_size = args.step_size
+filter_index = args.filter_index
+
 
 def normal(img):
     if np.ndim(img) == 4:
@@ -44,7 +52,7 @@ submodel = tf.keras.models.Model([model.inputs[0]], [model.get_layer(layer_name)
 
 # Initiate random noise
 input_img_data = np.random.random((1, 224, 224, 3))
-#input_img_data = (input_img_data - 0.5) * 20 + 128.
+input_img_data = (input_img_data - 0.5) * 20 + 128.
 
 # Cast random noise from np.float64 to tf.float32 Variable
 input_img_data = tf.Variable(tf.cast(input_img_data, tf.float32))
@@ -64,13 +72,13 @@ for i in range(epochs):
 img_norm = normal(input_img_data.numpy())
 img_hist = hisEqulColor(img_norm)
 
-'''
+
 cv2.imshow("img_norm", img_norm)
 cv2.imshow("img_hist", img_hist)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
 
+cv2.imwrite('kernel_norm.jpg', img_norm)
 
 
 
